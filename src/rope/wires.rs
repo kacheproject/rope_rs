75fa2de9ec3c024594d4468574e78f85c3d04fed
thisLight <l1589002388@gmail.com>
2022-04-15 14:23:21 +0800

    transports: move inproc to sparate mod

--- src/rope/wires.rs
+++ src/rope/wires.rs
@@ -7,7 +7,6 @@ use tokio::net::UdpSocket;
 use tokio::sync::{mpsc, Mutex};
 
 
-use super::netmeter::NetworkMeter;
 use super::ExternalAddr;
 
 #[async_trait]
@@ -19,66 +18,9 @@ pub trait Tx: Debug + Send + Sync {
     fn is_removable(&self) -> bool;
 }
 
-#[derive(Debug)]
-pub struct ChanPairTx { tx: mpsc::Sender<Box<[u8]>> }
-
-impl ChanPairTx {
-    pub fn new(tx: mpsc::Sender<Box<[u8]>>) -> Self {
-        Self {
-            tx,
-        }
-    }
-}
-
-#[async_trait]
-impl Tx for ChanPairTx {
-    async fn send_to(&self, buf: &[u8]) -> io::Result<usize> {
-        let tx = &self.tx;
-        let boxed_slice = Vec::from(buf).into_boxed_slice();
-        let size = buf.len();
-        match tx.send(boxed_slice).await {
-            Ok(_) => Ok(size),
-            Err(_) => Err(io::Error::from(io::ErrorKind::BrokenPipe)),
-        }
-    }
-
-
-    fn get_availability(&self) -> f64 {
-        if !self.tx.is_closed() {
-            1.0
-        } else {
-            0.0
-        }
-    }
-
-
-    fn is_removable(&self) -> bool {
-        self.tx.is_closed()
-    }
-
-}
-
 #[async_trait]
 pub trait Rx: Debug + Send + Sync {
     async fn recv_from(&self, buf: &mut [u8]) -> io::Result<(usize, ExternalAddr)>;
-}
-
-pub type ChanPairRx = Arc<Mutex<mpsc::Receiver<Box<[u8]>>>>;
-
-#[async_trait]
-impl Rx for ChanPairRx {
-    async fn recv_from(&self, buf: &mut [u8]) -> io::Result<(usize, ExternalAddr)> {
-        let mut rx = self.lock().await;
-        if let Some(data) = rx.recv().await {
-            let copy_size = std::cmp::min(data.len(), buf.len());
-            for i in 0..copy_size {
-                buf[i] = data[i]
-            }
-            Ok((data.len(), ExternalAddr::None))
-        } else {
-            Err(io::Error::from(io::ErrorKind::BrokenPipe))
-        }
-    }
 }
 
 /// Transport is for two-way connection-less data delivery methods.
@@ -100,20 +42,4 @@ where
 {
     fn create_tx(&self) -> Box<dyn Tx>;
     fn create_rx(&self) -> Box<dyn Rx>;
-}
-
-#[derive(Clone, Debug)]
-pub struct InprocTransport {
-    sender: mpsc::Sender<Box<[u8]>>,
-    receiver: Arc<Mutex<mpsc::Receiver<Box<[u8]>>>>,
-}
-
-impl ConnectedTransport for InprocTransport {
-    fn create_tx(&self) -> Box<dyn Tx> {
-        Box::new(ChanPairTx::new(self.sender.clone()))
-    }
-
-    fn create_rx(&self) -> Box<dyn Rx> {
-        Box::new(self.receiver.clone())
-    }
 }
